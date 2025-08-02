97ea2f42e5d2e5d00c0a1dea2449112f4179464a
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2025-08-02 23:36:04 +0530

    Complete UI of the Registration Form

---
+++ Assets/Scripts/ChangeField.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEditor.VersionControl;
+
+public class ChangeField : MonoBehaviour
+{
+    EventSystem system;
+    public Button submitbutton;
+    public Selectable firstInput;
+ 
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
+        {
+            Selectable pervious = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if(pervious != null)
+            {
+                pervious.Select();
+            }
+        }else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if(next != null)
+            {
+                next.Select();
+            } 
+        }else if (Input.GetKeyDown(KeyCode.Return)){
+            submitbutton.onClick.Invoke();
+        }
+    }
+}
