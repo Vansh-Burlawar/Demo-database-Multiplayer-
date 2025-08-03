f52d18c5123d92cddc12018917b5c40ea47a033e
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2025-08-03 23:41:57 +0530

    Registration Ongoing

---
+++ Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using UnityEditor.PackageManager;
+using TMPro;
+
+
+public class PlayFabManager : MonoBehaviour
+{
+
+    public TMP_Text Textmessage;
+    public TMP_InputField EmailText;
+    public TMP_InputField PasswordText;
+
+    
+   public void RegisterButton()
+    {
+        if(PasswordText.text.Length < 4)
+        {
+            Textmessage.text = "Its a small password ";
+            return;
+        }
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = EmailText.text,
+            Password = PasswordText.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterError);
+    }
+
+    public void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Textmessage.text="Registration Successful !!!";
+    }
+
+    public void OnRegisterError(PlayFabError error)
+    {
+        Textmessage.text = error.ErrorMessage;
+    }
+     public void LoginButton()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
