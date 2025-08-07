f19899c95b31c239e80154c8c601bc5a4b5a2b95
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2025-08-07 20:58:36 +0530

    Network manager code

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Host()
     {
-        
+       
+        NetworkManager.Singleton.StartHost();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Client()
     {
-        
+        NetworkManager.Singleton.StartClient();
     }
 }
